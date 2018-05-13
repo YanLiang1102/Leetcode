6cc0952bfd49953fd40e03740fc0764345d9ba8c
yan <yliang@ou.edu>
2018-05-13 09:49:04 -0500

    the array there is so superfluous we don't need that to track the order we just need to track and keep compare the last element

--- code-05122018/Leetcode/src/IsValidBST.java
+++ code-05122018/Leetcode/src/IsValidBST.java
@@ -3,41 +3,48 @@ import java.util.*;
 
 public class IsValidBST {
 	// Definition for a binary tree node.
-
+	boolean global=true;
 	public boolean isValid(TR root) {
 
 		ArrayList<Integer> arr = new ArrayList<Integer>();
-		copyBST(root,arr);
-		for (int i = 0; i < arr.size() - 1; i++) {
-			if (arr.get(i) < arr.get(i + 1))
-				continue;
-			else
-				return false;
-		}
-		return true;
+		int lastone=Integer.MIN_VALUE;
+		return copyBST(root,lastone);
 	}
-
-	private void copyBST(TR root, ArrayList<Integer> arr) {
+   
+	private boolean copyBST(TR root,int lastone) {
+		//System.out.println("I got here!");
 		if (root == null)
-			return;
-		copyBST(root.left, arr);
-		arr.add(root.val);
+			return true;
 		System.out.println(root.val);
-		copyBST(root.right, arr);
+		if(!copyBST(root.left,lastone))
+			return false;
+		if(root.val<lastone)
+			return false;
+		else
+			lastone=root.val;
+		    System.out.println("lastone: "+lastone);
+		if(!copyBST(root.right,lastone))
+			return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
 		IsValidBST solution=new IsValidBST();
 		TR n5 = new TR(5);
 		TR n1 = new TR(1);
-		TR n4 = new TR(4);
+		TR n4 = new TR(2);
 		TR n3 = new TR(3);
-		TR n6 = new TR(6);
+		TR n6 = new TR(7);
+		TR n7= 	new TR(6);
+		TR n8= 	new TR(9);
 		
 		n5.left=n1;
 		n5.right=n4;
 		n4.left=n3;
 		n4.right=n6;
+		n6.left=n7;
+		n6.right=n8;
+			
 		
 		//n5 is the starting node.
 		if(solution.isValid(n5)) {
