5cd8aafecc0f31c7ec9560704845fbb44b59e03d
YanLiang <yliang@ou.edu>
2018-05-20 11:32:45 -0500

    Add in the BST sequemce code
    Implement two methods to get "weave" list and one of the add(empty) list is necessary , otherwise, the loop will not get run, and I saw this before, this is very similar to what I have seen in CreateStringPermutationWIthNoDup.java

--- code-05122018/Leetcode/src/WeaveList.java
+++ code-05122018/Leetcode/src/WeaveList.java
@@ -2,54 +2,95 @@ import java.io.*;
 import java.util.*;
 import java.util.LinkedList;
 
-public class WeaveList {
-
-	//this is one of the function for the problem on cracking the interview page 263 BST sequence problem,
-	//this is to weave two list together but remain the order of each original list.
+public class NQueens {
 	
-    public static ArrayList<ArrayList<Integer>> weaveList(ArrayList<ArrayList<Integer>> result,ArrayList<Integer> one, ArrayList<Integer>list1,ArrayList<Integer>list2,
-    		int size, int pos1,int pos2)
-    {
-    	if(one.size()==size)
-    	{
-    		result.add(new ArrayList<Integer>(one));
-    	}
-    	for(int i=pos1;i<list1.size();i++)
-    	{
-           one.add(list1.get(i));
-           weaveList(result,one,list1,list2,size,i+1,pos2);
-           one.remove(one.size()-1);    		
-    	}
-    	for(int i=pos2;i<list2.size();i++)
-    	{
-    		one.add(list2.get(i));    		
-            weaveList(result,one,list1,list2,size,pos1,i+1);
-            one.remove(one.size()-1);
-    	}
-    	
-    	return result;
-    	
-    }
+	
+	// main idea: each columns configuration will map out to a ArrayList<Strin
+	public static void helper(ArrayList<ArrayList<String>> result,ArrayList<Integer> cols, int n)
+	{
+		if(cols.size()==n)
+		{
+		    result.add(drawBoard(cols,n));
+			return;
+		}
+		//otherwise recursively explore what the cols will look like.
+		for(int i=0;i<n;i++)
+		{
+			if(!isValid(cols,i))
+			{
+				continue;
+			}
+			else {
+				cols.add(i);
+				helper(result,cols,n);
+				cols.remove(cols.size()-1);
+			}
+		}		
+	}
+	
+	//using the the cols info to draw the solution out
+	public static ArrayList<String> drawBoard(ArrayList<Integer> cols,int n)
+	{
+		
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i=0;i<n;i++)
+		{
+			String temp="";
+			int col=cols.get(i);
+			for(int j=0;j<n;j++)
+			{
+				if(j!=col)
+				{
+					temp+=".";
+				}
+				else {
+					temp+="q";
+				}
+			}
+			result.add(temp);
+		}
+		return result;
+	}
+	
+	//cols will store what is the col quene is at ith row, j=cols.get(i) means on the ith row the queen will be at column j
+	public static boolean isValid(ArrayList<Integer> cols, int col)
+	{
+		int size=cols.size();
+		for(int i=0;i<size;i++)
+		{
+		   //col is on which posion the new queen is going to be added
+		   if(cols.get(i)==col)
+		   {
+			   return false;
+		   }		 		  
+		   //assume that it will be added to the next row
+		   //Math.abs is necessary!!! since the diagnal could be on 4 corners if the previous queen is on the right up corner of the current 
+		   //queen, then you will definitely need this!
+		   if(Math.abs(i-size)==Math.abs(cols.get(i)-col))
+		   {
+			   //this means they are on the same diagonal.
+			   return false;
+		   }
+		}
+		//otherwise putting a queen on this column and row is allowed!
+		return true;
+		
+	}
 	
 	public static void main(String[] args)
 	{
-		ArrayList<Integer> l1=new ArrayList<Integer>();
-		ArrayList<Integer> l2=new ArrayList<Integer>();
-		
-		l1.add(1);
-		l1.add(2);
-		l2.add(3);
-		l2.add(4);
-		
-		ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
-				weaveList(result,new ArrayList<Integer>(),l1,l2,4,0,0);
-		for(ArrayList<Integer> l:result)
-		{
-			for(int i:l)
-			{
-				System.out.print(i+",");
-			}
-			System.out.println("end");
-		}
+	  ArrayList<ArrayList<String>> result=new ArrayList<ArrayList<String>>();
+	  ArrayList<Integer> cols=new ArrayList<Integer>();
+	 	  
+	 helper(result,cols,5);
+	 for(ArrayList<String> l:result)
+	 {
+		 for(String s:l)
+		 {
+			 System.out.print(s);
+		 }
+		 System.out.println("end");
+	 }
+	  
 	}
 }
