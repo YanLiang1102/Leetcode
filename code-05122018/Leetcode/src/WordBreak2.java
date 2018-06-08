dbf5e951e6cb4a20e7bcb5afc493f8caf4b58a34
YanLiang <yliang@ou.edu>
2018-06-08 12:52:25 -0500

    Create WordBreak2.java
    return all the possible break up of the string. this is a hard problem on Leetcode, I wrote it all by myself, using backtrack idea, but this test could not pass
    	//this test can not be passed why, how to fix this problem.
    //	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
    	//["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]

---
+++ code-05122018/Leetcode/src/WordBreak2.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class WordBreak2 {
+
+	//print out first
+	public static void getAllSolutions(String s, List<String> wordDict,ArrayList<ArrayList<String>> result, ArrayList<String> one,int index) {
+		
+		if(index==s.length())
+		{
+			result.add(new ArrayList<String>(one));
+			return;
+		}
+ 		for(int i=index;i<=s.length();i++)
+		{
+			String curr=s.substring(index, i);
+			
+			if(wordDict.contains(curr))
+			{
+				one.add(curr);				
+				getAllSolutions(s,wordDict,result, new ArrayList<String>(one),i);	
+				one.remove(one.size()-1);
+			}
+			
+		}
+	}
+	
+	//this test can not be passed why, how to fix this problem.
+//	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	//["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+	
+
+	public static void main(String[] args) {
+		ArrayList<ArrayList<String>> result=new ArrayList<ArrayList<String>>();
+		ArrayList<String> one=new ArrayList<String>();
+
+		List<String> wordDict=new ArrayList<String>();
+//		String s="catsanddog";
+//		wordDict.add("cat");
+//		wordDict.add("cats");
+//		wordDict.add("and");
+//		wordDict.add("sand");
+//		wordDict.add("dog");
+		
+//		String s="pineapplepenapple";
+//		wordDict.add("apple");
+//		wordDict.add("pen");
+//		wordDict.add("applepen");
+//		wordDict.add("pine");
+//		wordDict.add("pineapple");
+	
+//		String s="catmn";
+//	    wordDict.add("cat");
+//	    wordDict.add("m");
+//	    wordDict.add("n");
+//	    wordDict.add("mn");
+		
+		//String s="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		String s="aaaaa";
+		wordDict.add("a");
+		wordDict.add("aa");
+		wordDict.add("aaa");
+		wordDict.add("aaaa");
+		wordDict.add("aaaaa");
+		wordDict.add("aaaaaa");
+		wordDict.add("aaaaaaa");
+		wordDict.add("aaaaaaaa");
+		wordDict.add("aaaaaaaaa");
+		wordDict.add("aaaaaaaaaa");
+	
+		getAllSolutions(s,wordDict,result,one,0);
+		for(ArrayList<String>item:result)
+		{
+		    for(String i:item)
+		    {
+		    	System.out.print(i+",");
+		    }
+		    System.out.println("");
+		}
+		
+		
+		
+	}
+
+}
