b0ad952a2aa575d5b96f2cee87a26d078dff9189
YanLiang <yliang@ou.edu>
2018-06-19 12:53:11 -0500

    Update WordBreak2.java
    word break with memorization.

--- code-05122018/Leetcode/src/WordBreak2.java
+++ code-05122018/Leetcode/src/WordBreak2.java
@@ -1,28 +1,94 @@
 import java.util.*;
 
 public class WordBreak2 {
-
-	//using dfs to find all the solutions. this can be a helper method on leetcode, 
-	//since the parameter required on leetcode is only "String s and List<String> wordDict".
-	public static void getAllSolutions(String s, List<String> wordDict,ArrayList<ArrayList<String>> result, ArrayList<String> one,int index) {
-		
+	
+	//thsi is only dfs without memeorization will get time out
+	public static void getAllSolutions(String s,List<String>wordDict,ArrayList<ArrayList<String>>result, ArrayList<String> one, int index)
+	{
 		if(index==s.length())
 		{
 			result.add(new ArrayList<String>(one));
 			return;
 		}
- 		for(int i=index;i<=s.length();i++)
+		for(int i=index;i<s.length();i++)
 		{
-			String curr=s.substring(index, i);
-			
+			String curr=s.substring(index, i+1);
 			if(wordDict.contains(curr))
 			{
-				one.add(curr);				
-				getAllSolutions(s,wordDict,result, new ArrayList<String>(one),i);	
+				one.add(curr);
+				//this has to be i+1, in that way that index==s.length() can be passed otherwise, it will never get passed.
+				getAllSolutions(s,wordDict,result,one,i+1);
 				one.remove(one.size()-1);
 			}
-			
 		}
+		
+	}
+	//using dfs with memorization will solve the time out problem.
+	public static List<String> dfsWithMemorization(String s, List<String>wordDict, HashMap<String,List<String>>map)
+	{
+		if(map.containsKey(s))
+		{
+			return map.get(s);
+		}
+		
+		List<String> res=new ArrayList<String>();
+		for(String word:wordDict)
+		{
+			if(s.startsWith(word))
+			{
+				String remaining=s.substring(word.length());
+				List<String>temp=dfsWithMemorization(remaining, wordDict,map);
+				//thsi means that when the string is "", the res will be empty so the loop will not run at all, so nothing will be added in , so we need this check.
+				if(temp.isEmpty())
+				{
+					res.add(word+" ");
+				}
+				else
+				{
+					for(String item:temp)
+					{
+						res.add(word+" "+item);
+					}
+
+				}
+			}
+		}
+		map.put(s, res);
+		return res;
+	}
+	
+	public static List<String> dfsWithMemorization2(String s, List<String>wordDict, HashMap<String,List<String>>map)
+	{
+		if(map.containsKey(s))
+		{
+			return map.get(s);
+		}
+		
+		List<String> res=new ArrayList<String>();
+		//this is another strategy to solve the problem.
+		if(s.equals(""))
+		{
+			res.add("");
+			return res;
+		}
+		for(String word:wordDict)
+		{
+			if(s.startsWith(word))
+			{
+				String remaining=s.substring(word.length());
+				List<String>temp=dfsWithMemorization(remaining, wordDict,map);
+				//thsi means that when the string is "", the res will be empty so the loop will not run at all, so nothing will be added in , so we need this check.
+
+					for(String item:temp)
+					{
+						res.add(word+" "+item);
+					}
+
+			
+			}
+		}
+		map.put(s, res);
+		return res;
 	}
 	
 	//this test can not be passed why, how to fix this problem.
@@ -42,12 +108,12 @@ public class WordBreak2 {
 //		wordDict.add("sand");
 //		wordDict.add("dog");
 		
-//		String s="pineapplepenapple";
-//		wordDict.add("apple");
-//		wordDict.add("pen");
-//		wordDict.add("applepen");
-//		wordDict.add("pine");
-//		wordDict.add("pineapple");
+		String s="pineapplepenapple";
+		wordDict.add("apple");
+		wordDict.add("pen");
+		wordDict.add("applepen");
+		wordDict.add("pine");
+		wordDict.add("pineapple");
 	
 //		String s="catmn";
 //	    wordDict.add("cat");
@@ -55,28 +121,25 @@ public class WordBreak2 {
 //	    wordDict.add("n");
 //	    wordDict.add("mn");
 		
-		//String s="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-		String s="aaaaa";
-		wordDict.add("a");
-		wordDict.add("aa");
-		wordDict.add("aaa");
-		wordDict.add("aaaa");
-		wordDict.add("aaaaa");
-		wordDict.add("aaaaaa");
-		wordDict.add("aaaaaaa");
-		wordDict.add("aaaaaaaa");
-		wordDict.add("aaaaaaaaa");
-		wordDict.add("aaaaaaaaaa");
-	
-		getAllSolutions(s,wordDict,result,one,0);
-		for(ArrayList<String>item:result)
+		HashMap<String, List<String>>map=new HashMap<String,List<String>>();
+		List<String> res=dfsWithMemorization2(s,wordDict,map);
+		for(String str:res)
 		{
-		    for(String i:item)
-		    {
-		    	System.out.print(i+",");
-		    }
-		    System.out.println("");
+			System.out.println(str);
 		}
+	
+//		getAllSolutions(s,wordDict,result,one,0);
+//		for(ArrayList<String>item:result)
+//		{
+//		    for(String i:item)
+//		    {
+//		    	System.out.print(i+",");
+//		    }
+//		    System.out.println("");
+//		}
+//		
+//		String test="hello oklahoma";
+//		System.out.println(test.substring(4));
 		
 		
 		
