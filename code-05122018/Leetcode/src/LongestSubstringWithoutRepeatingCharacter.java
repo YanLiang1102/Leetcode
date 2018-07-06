d3e9961b634cfeb87e0ad1713bf1f90ad76b3cf1
YanLiang <yliang@ou.edu>
2018-07-06 13:14:21 -0500

    Create LongestSubstringWithoutRepeatingCharacter.java

---
+++ code-05122018/Leetcode/src/LongestSubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s==null||s.length()==0)
+        {
+            return 0;
+        }
+        if(s.length()==1)
+        {
+            return 1;
+        }
+        char [] arr=s.toCharArray();
+        int[] m=new int[s.length()];
+        //abcabcbb
+        m[0]=1;
+        int globalmax=0;
+        for(int i=1;i<s.length();i++)
+        {
+            char c=arr[i];
+            int index=s.substring(i-m[i-1],i).indexOf(c);
+            if(index<0)
+            {
+                m[i]=m[i-1]+1;
+            }
+            else
+            {
+                m[i]=i-index;
+            }
+            globalmax=Math.max(globalmax,m[i]);
+        }
+        
+        return globalmax;
+    }
+}
