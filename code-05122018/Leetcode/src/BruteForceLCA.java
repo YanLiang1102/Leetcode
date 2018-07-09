f97e8d0fe362645283f401ca0510a5e89b8b418b
YanLiang <yliang@ou.edu>
2018-07-09 15:14:22 -0500

    Update BruteForceLCA.java

--- code-05122018/Leetcode/src/BruteForceLCA.java
+++ code-05122018/Leetcode/src/BruteForceLCA.java
@@ -16,17 +16,34 @@ class Solution {
         {
             return root;
         }
-        if(isParent(root.left,p)&&isParent(root.left,q))
-        {
-            return lowestCommonAncestor(root.left,p,q);
-        }
-        else if(isParent(root.right,p)&&isParent(root.right,q))
-        {
-            return lowestCommonAncestor(root.right,p,q);
-        }
-        else if((isParent(root.left,p)&&isParent(root.right,q))||(isParent(root.right,p)&&isParent(root.left,q)))
+//         if(isParent(root.left,p)&&isParent(root.left,q))
+//         {
+//             return lowestCommonAncestor(root.left,p,q);
+//         }
+//         else if(isParent(root.right,p)&&isParent(root.right,q))
+//         {
+//             return lowestCommonAncestor(root.right,p,q);
+//         }
+//         else if((isParent(root.left,p)&&isParent(root.right,q))||(isParent(root.right,p)&&isParent(root.left,q)))
+//         {
+//             return root;
+//         }
+//         return null;
+     //you don't need to check isParent, change to check if it is null or not will help check if it is parent, but
+     //help you save a lot of time! and this become a much better solution.
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null&&right!=null)
         {
             return root;
+        }
+        else if(left!=null)
+        {
+            return left;
+        }
+        else if(right!=null)
+        {
+            return right;
         }
         return null;
     }
