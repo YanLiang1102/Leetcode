4d4111c8649b48d3ab2cb4b9ec5bdb335e3cf2da
YanLiang <yliang@ou.edu>
2018-07-13 09:40:22 -0500

    Create MergeTwoBinaryTree.java

---
+++ code-05122018/Leetcode/src/MergeTwoBinaryTree.java
@@ -0,0 +1,45 @@
+/****
+using recursion to do this and i get a one time pass there, yeh!
+****/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    //class doubleNode
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        
+        TreeNode t;
+        if(t1==null)
+        {
+            return t2;
+        }
+        if(t2==null)
+        {
+            return t1;
+        }
+        
+        TreeNode left1=mergeTrees(t1.left, t2.left);
+        TreeNode right1=mergeTrees(t1.right, t2.right);
+        
+        int value=getValue(t1)+getValue(t2);
+        t=new TreeNode(value);
+        t.left=left1;
+        t.right=right1;
+        return t;
+    }
+    
+    public int getValue(TreeNode t)
+    {
+        if(t==null)
+            return 0;
+        else 
+            return t.val;
+    }
+}
